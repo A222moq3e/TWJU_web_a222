809390aa37440c20a2f1eebadb8bbbe56f0b27df
Abdullah-d3v <moq3e2000@gmail.com>
2025-09-05 16:14:28 +0300

    trying solve production web

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,11 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 3000,
     strictPort: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: process.env.VITE_API_URL || 'http://localhost:3001',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
