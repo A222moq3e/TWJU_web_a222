95b0d0f3c2ee52533ace11eb7ed29b0584d9229b
Abdullah-d3v <moq3e2000@gmail.com>
2025-09-13 13:55:10 +0300

    dissable obfuscator plugin

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // install before use:
 // npm i -D vite-plugin-javascript-obfuscator javascript-obfuscator terser
 
-import obfuscatorPlugin from 'vite-plugin-javascript-obfuscator'
+// import obfuscatorPlugin from 'vite-plugin-javascript-obfuscator'
 
 export default defineConfig(({ command }) => {
   const isBuild = command === 'build'
@@ -13,25 +13,25 @@ export default defineConfig(({ command }) => {
   return {
     plugins: [
       react(),
-      ...(isBuild ? [
-        obfuscatorPlugin({
-          // prefer to exclude vendor and node_modules; plugin may accept regex or globs
-          // if the plugin doesn't support 'exclude' you can remove this and rely on manualChunks
-          exclude: ['**/vendor.*.js', /node_modules/],
-          // options forwarded to javascript-obfuscator
-          options: {
-            compact: true,
-            controlFlowFlattening: true,
-            controlFlowFlatteningThreshold: 0.75,
-            deadCodeInjection: false,
-            debugProtection: false,
-            identifierNamesGenerator: 'hexadecimal',
-            stringArray: true,
-            stringArrayEncoding: ['base64'],
-            // DO NOT enable sourceMap
-          }
-        })
-      ] : [])
+      // ...(isBuild ? [
+      //   obfuscatorPlugin({
+      //     // prefer to exclude vendor and node_modules; plugin may accept regex or globs
+      //     // if the plugin doesn't support 'exclude' you can remove this and rely on manualChunks
+      //     exclude: ['**/vendor.*.js', /node_modules/],
+      //     // options forwarded to javascript-obfuscator
+      //     options: {
+      //       compact: true,
+      //       controlFlowFlattening: true,
+      //       controlFlowFlatteningThreshold: 0.75,
+      //       deadCodeInjection: false,
+      //       debugProtection: false,
+      //       identifierNamesGenerator: 'hexadecimal',
+      //       stringArray: true,
+      //       stringArrayEncoding: ['base64'],
+      //       // DO NOT enable sourceMap
+      //     }
+      //   })
+      // ] : [])
     ],
     server: {
       host: '0.0.0.0',
