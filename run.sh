51d6b0c29d35fd7f0132e1e7978d4dbc7b5d8cb9
Abdullah-d3v <moq3e2000@gmail.com>
2025-09-28 20:36:54 +0300

    implement some requirements

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# Initialize database and seed data
+cd /srv/app/server
+rm -rf prisma/migrations
+npx prisma db push
+node dist/seed.js
+
 # Start the Node.js application on port 5000 in the background
-(cd /srv && node app/server/dist/server.js)&
+cd /srv
+node app/server/dist/server.js &
 
 # Use socat to redirect HTTP traffic to the service running on port 5000
 socat - TCP:127.0.0.1:5000,forever
