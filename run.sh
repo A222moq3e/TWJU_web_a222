16501a3759990e429d2d48b2d94fd3541b62e635
Abdullah-d3v <moq3e2000@gmail.com>
2025-09-28 20:33:29 +0300

    implement some requirements

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Start the Node.js application on port 5000 in the background
-(cd /srv/app && node server/dist/server.js)&
+(cd /srv && node app/server/dist/server.js)&
 
 # Use socat to redirect HTTP traffic to the service running on port 5000
 socat - TCP:127.0.0.1:5000,forever
